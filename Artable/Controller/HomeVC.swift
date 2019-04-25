cad1d090b4c7750f078da51ccdc5e755c0bdf796
Jay <jay.ambrish.raval@gmail.com>
2019-04-25 19:36:55 -0400

    anonymous login and linking

---
+++ Artable/Controller/HomeVC.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Jay Raval on 4/22/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class HomeVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var logInOutButton: UIBarButtonItem!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if Auth.auth().currentUser == nil {
+            Auth.auth().signInAnonymously { (result, error) in
+                if let error = error {
+                    debugPrint(error)
+                }
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let user = Auth.auth().currentUser, !user.isAnonymous {
+            // We are logged in
+            logInOutButton.title = "Logout"
+        } else {
+            logInOutButton.title = "Login"
+        }
+    }
+    
+    fileprivate func presentLoginController() {
+        let storyboard = UIStoryboard(name: Storyboard.LoginStoryboard, bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: StoryboardID.LoginVC)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func logInOutButton(_ sender: Any) {
+        
+        guard let user = Auth.auth().currentUser else { return }
+        if user.isAnonymous {
+            presentLoginController()
+        } else {
+            do {
+                try Auth.auth().signOut()
+                Auth.auth().signInAnonymously { (result, error) in
+                    if let error = error {
+                        debugPrint(error)
+                    }
+                    self.presentLoginController()
+                }
+            } catch {
+                debugPrint(error)
+            }
+        }
+    }
+}
