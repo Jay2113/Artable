3eeba922709500603d4397729d2fc1d5d6cec2d7
Jay <jay.ambrish.raval@gmail.com>
2019-05-08 02:02:21 -0400

    Adding document changes, queries and ordering them.

--- Artable/Controller/HomeVC.swift
+++ Artable/Controller/HomeVC.swift
@@ -26,10 +26,17 @@ class HomeVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         db = Firestore.firestore()
+        setupCollectionView()
+        setupInitialAnonymousUser()
+    }
+
+    func setupCollectionView() {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: Identifiers.CategoryCell, bundle: nil), forCellWithReuseIdentifier: Identifiers.CategoryCell)
-        
+    }
+    
+    func setupInitialAnonymousUser() {
         if Auth.auth().currentUser == nil {
             Auth.auth().signInAnonymously { (result, error) in
                 if let error = error {
@@ -40,36 +47,8 @@ class HomeVC: UIViewController {
         }
     }
     
-    func fetchDocument() {
-        let documentReference = db.collection("categories").document("YlwuP8qeU8iC53NciNif")
-        
-        listener = documentReference.addSnapshotListener { (snap, error) in
-            self.categories.removeAll()
-            guard let data = snap?.data() else { return }
-            let newCategory = Category.init(data: data)
-            self.categories.append(newCategory)
-            self.collectionView.reloadData()
-        }
-    }
-    
-    func fetchCollection() {
-        let collectionReference = db.collection("categories")
-        
-        listener = collectionReference.addSnapshotListener { (snap, error) in
-            guard let documents = snap?.documents else { return }
-            self.categories.removeAll()
-            for document in documents {
-                let data = document.data()
-                let newCategory = Category.init(data: data)
-                self.categories.append(newCategory)
-            }
-            self.collectionView.reloadData()
-        }
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
-        //        fetchDocument()
-        fetchCollection()
+        setCategoriesListener()
         if let user = Auth.auth().currentUser, !user.isAnonymous {
             // We are logged in
             logInOutButton.title = "Logout"
@@ -80,6 +59,31 @@ class HomeVC: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         listener.remove()
+        categories.removeAll()
+        collectionView.reloadData()
+    }
+    
+    func setCategoriesListener() {
+        listener = db.categories.addSnapshotListener({ (snap, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                return
+            }
+            
+            snap?.documentChanges.forEach({ (change) in
+                let data = change.document.data()
+                let category = Category.init(data: data)
+                
+                switch change.type {
+                case .added:
+                    self.onDocumentAdded(change: change, category: category)
+                case .modified:
+                    self.onDocumentModified(change: change, category: category)
+                case .removed:
+                    self.onDocumentRemoved(change: change)
+                }
+            })
+        })
     }
     
     fileprivate func presentLoginController() {
@@ -112,6 +116,37 @@ class HomeVC: UIViewController {
 }
 
 extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func onDocumentAdded(change: DocumentChange, category: Category) {
+        let newIndex = Int(change.newIndex)
+        categories.insert(category, at: newIndex)
+        collectionView.insertItems(at: [IndexPath(item: newIndex, section: 0)])
+    }
+    
+    func onDocumentModified(change: DocumentChange, category: Category) {
+        if change.oldIndex == change.newIndex {
+            // Item changed, but remained in the same position
+            let index = Int(change.newIndex)
+            categories[index] = category
+            collectionView.reloadItems(at: [IndexPath(item: index, section: 0)])
+        } else {
+            // Item changed and changed position
+            let oldIndex = Int(change.oldIndex)
+            let newIndex = Int(change.newIndex)
+            categories.remove(at: oldIndex)
+            categories.insert(category, at: newIndex)
+            
+            collectionView.moveItem(at: IndexPath(item: oldIndex, section: 0), to: IndexPath(item: newIndex, section: 0))
+        }
+    }
+    
+    func onDocumentRemoved(change: DocumentChange) {
+        let oldIndex = Int(change.oldIndex)
+        categories.remove(at: oldIndex)
+        collectionView.deleteItems(at: [IndexPath(item: oldIndex, section: 0)])
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return categories.count
     }
@@ -145,3 +180,31 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
         }
     }
 }
+
+// Code for reference!
+//    func fetchDocument() {
+//        let documentReference = db.collection("categories").document("YlwuP8qeU8iC53NciNif")
+//
+//        listener = documentReference.addSnapshotListener { (snap, error) in
+//            self.categories.removeAll()
+//            guard let data = snap?.data() else { return }
+//            let newCategory = Category.init(data: data)
+//            self.categories.append(newCategory)
+//            self.collectionView.reloadData()
+//        }
+//    }
+//
+//    func fetchCollection() {
+//        let collectionReference = db.collection("categories")
+//
+//        listener = collectionReference.addSnapshotListener { (snap, error) in
+//            guard let documents = snap?.documents else { return }
+//            self.categories.removeAll()
+//            for document in documents {
+//                let data = document.data()
+//                let newCategory = Category.init(data: data)
+//                self.categories.append(newCategory)
+//            }
+//            self.collectionView.reloadData()
+//        }
+//    }
--- Artable/Controller/ProductsVC.swift
+++ Artable/Controller/ProductsVC.swift
@@ -17,22 +17,77 @@ class ProductsVC: UIViewController {
     // Variables
     var products = [Product]()
     var category: Category!
+    var listener: ListenerRegistration!
+    var db: Firestore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        let product = Product(name: "Landscape", id: "asdasad", category: "Nature", price: 24.99, productDescription: "Great Product!", imageURL: "https://images.unsplash.com/photo-1555979864-7a8f9b4fddf8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80", timeStamp: Timestamp(), stock: 0, favorite: false)
-        products.append(product)
+        db = Firestore.firestore()
+//        let product = Product(name: "Landscape", id: "asdasad", category: "Nature", price: 24.99, productDescription: "Great Product!", imageURL: "https://images.unsplash.com/photo-1555979864-7a8f9b4fddf8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80", timeStamp: Timestamp(), stock: 0)
+//        products.append(product)
         
         
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: Identifiers.ProductCell, bundle: nil), forCellReuseIdentifier: Identifiers.ProductCell)
+        
+        setupQuery()
+    }
+    
+    func setupQuery() {
+        listener = db.products(category: category.id).addSnapshotListener({ (snap, error) in
+            if let error = error {
+                debugPrint(error)
+            }
+            
+            snap?.documentChanges.forEach({ (change) in
+                let data = change.document.data()
+                let product = Product.init(data: data)
+                
+                switch change.type {
+                case .added:
+                    self.onDocumentAdded(change: change, product: product)
+                case .modified:
+                    self.onDocumentModified(change: change, product: product)
+                case .removed:
+                    self.onDocumentRemoved(change: change)
+                }
+            })
+        })
     }
 }
 
 extension ProductsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func onDocumentAdded(change: DocumentChange, product: Product) {
+        let newIndex = Int(change.newIndex)
+        products.insert(product, at: newIndex)
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .fade)
+    }
+    
+    func onDocumentModified(change: DocumentChange, product: Product) {
+        if change.oldIndex == change.newIndex {
+            // Item changed, but remained in the same position
+            let index = Int(change.newIndex)
+            products[index] = product
+            tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
+        } else {
+            // Item changed, changed the position
+            let oldIndex = Int(change.oldIndex)
+            let newIndex = Int(change.newIndex)
+            products.remove(at: oldIndex)
+            products.insert(product, at: newIndex)
+            tableView.moveRow(at: IndexPath(row: oldIndex, section: 0), to: IndexPath(row: newIndex, section: 0))
+        }
+    }
+    
+    func onDocumentRemoved(change: DocumentChange) {
+        let oldIndex = Int(change.oldIndex)
+        products.remove(at: oldIndex)
+        tableView.deleteRows(at: [IndexPath(row: oldIndex, section: 0)], with: .left)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return products.count
     }
